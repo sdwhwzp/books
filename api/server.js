779aa3073b46c9e62fb2583946cf491b02d68d03
wzp <sdwhwzp@126.com>
2019-07-19 20:00:08 +0800

    v0.1

--- api/server.js
+++ api/server.js
@@ -1,4 +1,7 @@
 const  express=require("express")
+const db=require("./module/db")
+const hp =require("./module/hp")
+const jwt =require("jwt-simple")
 const app = express()
 app.listen(80,function () {
     console.log("success")
